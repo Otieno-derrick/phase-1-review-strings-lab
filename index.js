4106e6001e7d9e8a65118f23392f3be1d63253e0
derrique <otieno.derrick@student.moringaschool.com>
2022-06-08 19:45:39 +0300

    commit tree 37b45590877c93548a868a46e5624426f218c4c1

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code in this file!
+const currentUser = "Derrick Ochieng"
+print (currentUser)
+
+const welcomeMessage = `${currentUser} Welcome to Flatbook, !`
+print (welcomeMessage)
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+print (excitedWelcomeMessage)
+
+const shortGreeting = `Welcome, `
